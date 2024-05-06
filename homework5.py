39860d675b5c001ee92e3467001cee340a0b4fc6
Alexey-nsk <168356430+Alexey-nsk@users.noreply.github.com>
2024-05-06 23:36:26 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list=['lemon','melon','peach','orange','grape']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2::])
+my_list[2]='banana'
+print(my_list)
+my_dict={'fox':'лиса','volf':'волк','wasp':'оса'}
+print(my_dict)
+print(my_dict['wasp'])
+del my_dict['wasp']
+my_dict['lion']='лев'
+print(my_dict)
